be3744d359631ba0c182c5bbc03643301952795d
BuildTools <unconfigured@null.spigotmc.org>
2018-05-15 13:16:51 -0700

    Clear before Import

--- src/java/nathan/apes/roots/Grouper.java
+++ src/java/nathan/apes/roots/Grouper.java
@@ -9,6 +9,7 @@ public class Grouper<T> extends ArrayList<T> {
     //Importing from regular
     //NOTE: Only works with empty Grouper's!
     public void importFromArray(T[] array){
+        clear();
         for(T element : array)
             add(element);
     }
